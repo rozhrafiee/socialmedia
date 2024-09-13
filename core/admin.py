eb1518e7b3a60196145a593520e01934d8867964
rozhrafiee <rozhinarafiee014@gmail.com>
2024-09-13 14:02:29 +0430

    miow

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import UserProfile, Post, Like, Comment
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'profile_pic')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'caption', 'created_at')
+    list_filter = ('created_at',)
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created_at')
+    list_filter = ('created_at',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'text', 'created_at')
+    list_filter = ('created_at',)
